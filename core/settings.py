fb0b0a14eb44c816b26522599f8b087d75a65746
Hailee Miu <hailee.miu@gmail.com>
2019-09-26 19:53:34 -0500

    create initial task model

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'life_app',
+    'task_app',
 ]
 
 MIDDLEWARE = [
--- task_app/models.py
+++ task_app/models.py
@@ -1,3 +1,10 @@
+""" Task App Models """
+
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+  """ Tasks to complete """
+  task_title = models.CharField(max_length=200)
+  task_description = models.TextField()
+  created_at = models.DateTimeField(auto_now_add=True)
+  completed_at = models.DateTimeField(blank=True)
