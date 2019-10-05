d53891e38736f6ceb5c67c11d89a59682c25b9fe
Hailee Miu <hailee.miu@gmail.com>
2019-10-05 10:37:39 -0400

    simple task detail view

--- task_app/urls.py
+++ task_app/urls.py
@@ -7,4 +7,5 @@ app_name = 'task_app'
 urlpatterns = [
     path('create/', views.TaskCreate.as_view(), name='task-create'),
     path('list/', views.TaskList.as_view(), name='task-list'),
+    path('<int:pk>/', views.TaskDetail.as_view(), name='task-detail'),
 ]
--- task_app/views.py
+++ task_app/views.py
@@ -2,18 +2,22 @@ from django.shortcuts import render
 from django.views.generic import edit
 from django.views import generic
 
-from task_app.models import Task
+from task_app import models
 from . import templates
 
 
 # TODO: add login required
 
 class TaskCreate(edit.CreateView):
-  model = Task
+  model = models.Task
   template_name = 'task_app/task_create.html'
   fields = ['task_title', 'task_description',]
 
 class TaskList(generic.ListView):
-  model = Task
+  model = models.Task
   template_name = 'task_app/task_list.html'
 
+class TaskDetail(generic.DetailView):
+  model = models.Task
+  template_name = 'task_app/task_detail.html'
+  context_object_name = 'task'
